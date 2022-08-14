e8b449d4c6794af8191f2a02326c0f04ebe0e7d4
Kadir-Yildiz <105437364+Kadir-Yildiz@users.noreply.github.com>
2022-08-15 00:35:44 +0300

    SliderManage

--- OtoServis.DataAccessLayer/DataContext.cs
+++ OtoServis.DataAccessLayer/DataContext.cs
@@ -4,11 +4,12 @@ using System.Data.Entity;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using OtoServis.Entities.Web;
 
 namespace OtoServis.DataAccessLayer
 {
     public class DataContext : DbContext
     {
-
+        public DbSet<Slider> Sliders { get; set; }
     }
 }
