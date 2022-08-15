8315f33cb865eb06605484e6f72548409ff35e57
Kadir-Yildiz <105437364+Kadir-Yildiz@users.noreply.github.com>
2022-08-15 23:39:12 +0300

    Slider

--- OtoServis.Web/Controllers/Web/SliderController.cs
+++ OtoServis.Web/Controllers/Web/SliderController.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using OtoServis.BusinessLayer.Abstract;
+using OtoServis.Entities.Web;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -8,10 +11,37 @@ namespace OtoServis.Web.Controllers.Web
 {
     public class SliderController : Controller
     {
+        private readonly Repository<Slider> rpSlider = new Repository<Slider>();
         // GET: Slider
         public ActionResult Index()
         {
-            return View();
+            var slider = rpSlider.List().OrderByDescending(x => x.SliderId);
+            return View(slider);
+        }
+        public ActionResult SliderKaydet(Slider slider, HttpPostedFileBase resim)
+        {
+            try
+            {
+                if (resim != null)
+                {
+                    string uzanti = Path.GetExtension(resim.FileName);
+                    string dosyaAdi = Path.GetFileNameWithoutExtension(resim.FileName) + "_" + Guid.NewGuid();
+                    string tamAd = dosyaAdi + uzanti;
+                    string yol = Server.MapPath("/Img/Slider/") + tamAd;
+                    resim.SaveAs(yol);
+                    string kaydedilecekYol = "/Img/Slider/" + tamAd;
+                    slider.Resim = kaydedilecekYol;
+                    rpSlider.Insert(slider);
+                    TempData["Ok"] = "Kayıt Başarılı";
+                }
+                return RedirectToAction("Index");
+            }
+            catch (Exception)
+            {
+                TempData["No"] = "Hata Oluştu";
+                return RedirectToAction("Index");
+            }
+
         }
     }
 }
