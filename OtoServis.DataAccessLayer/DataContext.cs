f7cb8e442f6e480a974a939ba1dca61f801ec637
Kadir-Yildiz <105437364+Kadir-Yildiz@users.noreply.github.com>
2022-08-17 23:43:11 +0300

    kampanya

--- OtoServis.DataAccessLayer/DataContext.cs
+++ OtoServis.DataAccessLayer/DataContext.cs
@@ -11,5 +11,6 @@ namespace OtoServis.DataAccessLayer
     public class DataContext : DbContext
     {
         public DbSet<Slider> Sliders { get; set; }
+        public DbSet<Kampanya> Kampanyas { get; set; }
     }
 }
