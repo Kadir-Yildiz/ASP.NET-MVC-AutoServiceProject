08fbb544a511834fb1f7e99e3050cba3e5c07880
Kadir-Yildiz <105437364+Kadir-Yildiz@users.noreply.github.com>
2022-08-20 13:50:58 +0300

    blogCRUD

---
+++ OtoServis.Web/Controllers/Web/BlogController.cs
@@ -0,0 +1,19 @@
+﻿using OtoServis.BusinessLayer.Abstract;
+using OtoServis.Entities.Web;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace OtoServis.Web.Controllers.Web
+{
+    public class BlogController : Controller
+    {
+        private readonly Repository<Blog> rpBlog = new Repository<Blog>();
+        public ActionResult Index()
+        {
+            return View(rpBlog.List().OrderByDescending(x=> x.BlogId));
+        }
+    }
+}
